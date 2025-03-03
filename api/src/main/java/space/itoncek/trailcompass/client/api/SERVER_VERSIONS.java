295c9f929e0fcce4327f0a47480eaf525fc7d515
Made By IToncek <itoncek3@itoncek.space>
2025-03-03 18:49:57 +0100

    Upversion! v0.0.0.6-dev

--- api/src/main/java/space/itoncek/trailcompass/client/api/SERVER_VERSIONS.java
+++ api/src/main/java/space/itoncek/trailcompass/client/api/SERVER_VERSIONS.java
@@ -1,5 +1,5 @@
 package space.itoncek.trailcompass.client.api;
 
 public class SERVER_VERSIONS {
-    public static final String SERVER_VERSION = "v0.0.0.4";
+    public static final String SERVER_VERSION = "v0.0.0.6";
 }
